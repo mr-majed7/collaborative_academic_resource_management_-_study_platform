d66e13ec038b1f14dc4ac219da74c2e80d826da7
majed <majedulislam7789@gmail.com>
2024-07-04 12:08:56 +0600

    Sort course and materials

--- src/controllers/viewC.js
+++ src/controllers/viewC.js
@@ -9,29 +9,44 @@ const LectureVideos = db.lectureVides
 module.exports.viewSildes = async (req, res)=> {
   const {folder_id} = req.params
   const slides = await Slide.findAll({where: {Folder_id: folder_id}})
+  if(req.params.sort){
+    slides.sort((a, b)=> a[req.params.sort] - b[req.params.sort])
+  }
   res.json(slides)
 }
 
 module.exports.viewBooks = async (req, res)=> {
   const {folder_id} = req.params
   const books = await Book.findAll({where: {Folder_id: folder_id}})
+  if(req.params.sort){
+    books.sort((a, b)=> a[req.params.sort] - b[req.params.sort])
+  }
   res.json(books)
 }
 
 module.exports.viewFolders = async (req, res)=> {
   const {user_id} = req.params
   const folders = await Folder.findAll({where: {User_id: user_id}})
+  if(req.params.sort){
+    folders.sort((a, b)=> a[req.params.sort] - b[req.params.sort])
+  }
   res.json(folders)
 }
 
 module.exports.viewLectureNotes = async (req, res)=> {
   const {folder_id} = req.params
   const lectureNotes = await LectreNote.findAll({where: {Folder_id: folder_id}})
+  if(req.params.sort){
+    lectureNotes.sort((a, b)=> a[req.params.sort] - b[req.params.sort])
+  }
   res.json(lectureNotes)
 }
 
 module.exports.viewLectureVideos = async (req, res)=> {
   const {folder_id} = req.params
   const lectureVideos = await LectureVideos.findAll({where: {Folder_id: folder_id}})
+  if(req.params.sort){
+    lectureVideos.sort((a, b)=> a[req.params.sort] - b[req.params.sort])
+  }
   res.json(lectureVideos)
 }
--- src/routes/viewRouter.js
+++ src/routes/viewRouter.js
@@ -6,12 +6,22 @@ const viewController = require('../controllers/viewC')
 
 router.get("/slides/:folder_id", viewController.viewSildes)
 
+router.get("/slides/:folder_id/:sort", viewController.viewSildes)
+
 router.get("/books/:folder_id", viewController.viewBooks)
+
+router.get("/books/:folder_id/:sort", viewController.viewBooks)
 
 router.get("/folders/:user_id", viewController.viewFolders)
 
+router.get("/folders/:user_id/:sort", viewController.viewFolders)
+
 router.get("/lectureNotes/:folder_id", viewController.viewLectureNotes)
 
+router.get("/lectureNotes/:folder_id/:sort", viewController.viewLectureNotes)
+
 router.get("/lectureVideos/:folder_id", viewController.viewLectureVideos)
+
+router.get("/lectureVideos/:folder_id/:sort", viewController.viewLectureVideos)
 
 module.exports = router
